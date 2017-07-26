4bf171d74c294fec9c522a19b8935eaa1f00a13e
Technohacker <technohacker1995@gmail.com>
2017-07-26 16:53:55 +0530

    Add the file this repository is all about

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# fun_code
-A dash of code spice for those days when coding can get drab
+# Fun code
+Feeling dull? Code looks all sad? Use `fun_code.h` and have a fun time working on your code!
+
+Contains `#defines` and `typedef`s for making code look lively
+
+Made while feeling bored in class
---
+++ fun_code.h
@@ -0,0 +1,22 @@
+#ifndef MAKE_CODING_FUN_AGAIN_H
+
+#define MAKE_CODING_FUN_AGAIN_H 1
+
+#define JUST_PRINT_FOR_GODS_SAKE cout
+#define GIMME_INPUT cin
+#define YOURE_WRONG cerr
+#define HERES_YOUR_DATA_JUST_GET_OUT_OF_MY_HAIR_ALREADY return
+#define HAS_THE_LEOPARD_BEEN_HIT kbhit()
+#define END_OF_THE_LINE ;
+#define UH_OH return 1
+#define DESTROY_THE_PROGRAM assert(false)
+#define AND_IT_GOES_ON_AND_ON_UNTIL while
+
+#define FAIR_DICE_THROW 4;
+
+#define where_students_reside class
+#define building struct
+
+typedef unsigned long long int really_large_number;
+
+#endif
